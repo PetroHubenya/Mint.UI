13af8a86c4681e4c93256b0c795d3a8b460da3f5
Petro Hubenya <p.hubenya@gmail.com>
2023-12-29 15:26:49 -0500

    The coins component is generated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoinsComponent } from './components/coins/coins.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoinsComponent
   ],
   imports: [
     BrowserModule,
