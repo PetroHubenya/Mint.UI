0c731381921911e844a86b18f10633394cb84e7f
Petro Hubenya <p.hubenya@gmail.com>
2023-12-29 15:33:19 -0500

    The coins component routing is configured in theapp-routing.module.ts file.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoinsComponent } from './components/coins/coins.component';
 
 const routes: Routes = [
   {
     path: '',
-    // component:
+    component: CoinsComponent
+  },
+  {
+    path: 'coins',
+    component: CoinsComponent
   }
 ];
 
