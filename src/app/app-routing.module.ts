e2cf6ea50fa66b75056840b1d9e4546bdc174c5a
Petro Hubenya <p.hubenya@gmail.com>
2024-01-10 21:41:01 -0500

    The path 'search/:searchString' is added to the app-routing.module.ts file.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoinsComponent } from './components/coins/coins.component';
 import { CoinComponent } from './components/coin/coin.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: 'coin/:id',
     component: CoinComponent
+  },
+  {
+    path: 'search/:searchString',
+    component: SearchComponent
   }
 ];
 
