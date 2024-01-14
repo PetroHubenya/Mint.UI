89cb35c4f5dadbad2a7670200493c966670c6de1
Petro Hubenya <p.hubenya@gmail.com>
2024-01-14 16:03:37 -0500

    The app.module.ts file is updated. The CommonModule and FormsModule are added. Everything works.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { CoinsComponent } from './components/coins/coins.component';
 import { CoinComponent } from './components/coin/coin.component';
 import { SearchComponent } from './components/search/search.component';
+
+
 
 @NgModule({
   declarations: [
@@ -16,6 +21,8 @@ import { SearchComponent } from './components/search/search.component';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
   ],
