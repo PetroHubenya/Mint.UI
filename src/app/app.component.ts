eab9531502703d9278f3df6199ff6efab7789852
Petro Hubenya <p.hubenya@gmail.com>
2024-01-15 22:01:59 -0500

    The Search form layout and details are defined in the AppComponent class.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Search } from './models/search.model';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Mint.UI';
 
-  
+  model = new Search('');
+
+  searchTest = '';
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
 
   
 }
